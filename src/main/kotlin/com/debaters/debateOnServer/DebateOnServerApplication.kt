2608048089474323f6c5665f9413dc89bef49dee
zimin <wlals822@naver.com>
2021-08-01 17:07:38 +0900

    Initial commit

---
+++ src/main/kotlin/com/debaters/debateOnServer/DebateOnServerApplication.kt
@@ -0,0 +1,11 @@
+package com.debaters.debateOnServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DebateOnServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DebateOnServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/debaters/debateOnServer/DebateOnServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.debaters.debateOnServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DebateOnServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
