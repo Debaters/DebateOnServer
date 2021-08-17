35028bd8cb5b2e4576eeddceadf587ed0cdc42f2
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-08-17 23:47:31 +0900

    spring-boot-starter-web 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,9 @@ repositories {
 extra["testcontainersVersion"] = "1.15.3"
 
 dependencies {
+
 //	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation ("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
