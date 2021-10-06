33b59b7e8181d8dc0e3507bbe195ed5345636c9e
wlals822 <wlals822@naver.com>
2021-10-06 21:23:40 +0900

    mongo db repository 도입 & 연동
    * mongo db repository를 사용하기 위한 설정 추가. configuration을 추가해서 db 설정을 코드에서 수행하도록 변경했습니다
    * debate관련 api들이 실제 데이터와 연동되는지 확인했습니다.
    * 더이상 사용하지 않는 template은 제거
    * debate 클래스에서 id 값의 이름은 debateId -> Id 로 변경

--- src/main/kotlin/com/debaters/debateOnServer/SpringConfig.kt
+++ src/main/kotlin/com/debaters/debateOnServer/SpringConfig.kt
@@ -1,7 +1,14 @@
 package com.debaters.debateOnServer
 
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
@@ -10,7 +17,6 @@ import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
-
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.CorsWebFilter
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
@@ -60,5 +66,26 @@ class SpringConfig {
         source.registerCorsConfiguration("/**", config)
 
         return CorsWebFilter(source)
+    }
+}
+
+@Configuration
+@EnableMongoRepositories(basePackages = ["com.debaters.debateOnServer.repositories"])
+class DBConfig : AbstractMongoClientConfiguration() {
+
+    @Value("\${spring.data.mongodb.uri}")
+    lateinit var mongoUri: String
+
+    override fun getDatabaseName(): String {
+        return "debates"
+    }
+
+    override fun mongoClient(): MongoClient {
+        val connectionString = ConnectionString(mongoUri)
+        val mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build()
+
+        return MongoClients.create(mongoClientSettings)
     }
 }
