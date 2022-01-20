dee63bf4e3fb6772bfdc64fd7728353365981262
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-01-21 01:02:43 +0900

    의존성 오류 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,8 @@ dependencies {
 
 	implementation("io.jsonwebtoken:jjwt-api:0.11.1")
 
-
+	implementation("io.sentry:sentry-spring-boot-starter:5.5.2")
+	implementation("io.sentry:sentry-logback:5.5.2")
 
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.1")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.1")
